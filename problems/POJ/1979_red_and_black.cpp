ce188a669a42a28929176faf75e4b7b88f46ad0c
Zhen Xiao <452576504@qq.com>
2018-10-24 20:19:59 +0800

    POJ_1979

--- problems/POJ/1979_red_and_black.cpp
+++ problems/POJ/1979_red_and_black.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    cout << "HELLO" << endl;
+char tiles[20][20];
+
+void printTiles()
+{
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            cout << tiles[i][j];
+        }
+        cout << endl;
+    } 
+}
+
+int main(int argc, char **argv) {
+    int W, H;
+
+    while (true) {
+        cin >> W >> H;
+        if (W == 0 && H == 0) break;
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                scanf("%c",&tiles[i][j]);
+            }
+        }
+        printTiles();
+    }
     return 0;
 }

ee190ea0a27cbb6cf5c47e5305fea520eb7565a9
xiaozhen <xz@xz.com>
2018-10-24 08:47:00 -0700

    POJ_1979_complete

--- problems/POJ/1979_red_and_black.cpp
+++ problems/POJ/1979_red_and_black.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 
 using namespace std;
 
-char tiles[20][20];
+char tiles[20][20]; // row column
+int W, H;
+vector<int> results;
 
 void printTiles()
 {
@@ -15,8 +18,28 @@ void printTiles()
     } 
 }
 
+int dfs(int curY, int curX) {
+    tiles[curY][curX] = '@';
+    int result = 1;
+
+    if (curX > 0 && tiles[curY][curX-1] == '.') {
+        result += dfs(curY, curX-1);
+    }
+    if (curY > 0 && tiles[curY-1][curX] == '.') {
+        result += dfs(curY-1, curX);
+    }
+    if (curX < W-1 && tiles[curY][curX+1] == '.') {
+        result += dfs(curY, curX+1);
+    }
+    if (curY < H-1 && tiles[curY+1][curX] == '.') {
+        result += dfs(curY+1, curX);
+    }
+
+    return result;
+}
+
 int main(int argc, char **argv) {
-    int W, H;
+    int startRow, startCol;
 
     while (true) {
         cin >> W >> H;
@@ -24,9 +47,18 @@ int main(int argc, char **argv) {
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
                 cin >> tiles[i][j];
+                if (tiles[i][j] == '@') {
+                    startRow = i;
+                    startCol = j;
+                }
             }
         }
+        results.push_back(dfs(startRow, startCol));
         //printTiles();
+    }
+
+    for (vector<int>::const_iterator it = results.begin(); it != results.end(); it++) {
+        cout << *it << endl;
     }
     return 0;
 }
